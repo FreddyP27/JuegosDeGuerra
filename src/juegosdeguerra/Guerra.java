7e0b0711103aa09e15f4b9800f834a7a08028b2b
Sergiop1908 <sergiop1908@gmail.com>
2025-02-20 20:49:03 +0100

    jueves20defebrero

---
+++ src/juegosdeguerra/Guerra.java
@@ -0,0 +1,16 @@
+package juegosdeguerra;
+
+
+public class Guerra {
+
+    public static void main(String[] args) {
+        
+        Guerra guerra = new Guerra();
+
+    }
+
+    private void llenarNaves(){
+        Guerrero guerrero1 = new Guerrero(null, null, 0, 0);
+    }
+
+}
