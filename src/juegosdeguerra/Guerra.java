d640fe9415252fed806e7786fc159726d6abe6c6
Sergiop1908 <sergiop1908@gmail.com>
2025-02-21 20:47:10 +0100

    Tanque(Vehiculo de guerra)

--- src/juegosdeguerra/Guerra.java
+++ src/juegosdeguerra/Guerra.java
@@ -5,10 +5,27 @@ public class Guerra {
 
     public static void main(String[] args) {
         
-        Guerra guerra = new Guerra();
+       Tanque tanque1 = new Tanque("T1", 20, 10);
 
+
+
+        int ataqueTanque = tanque1.atacar();
+
+        int defensaTanque = tanque1.defender(0);
+
+       System.out.println("El tanque esta realizando un ataque de :"+ataqueTanque);
+
+      
+
+       System.out.println("El tanque tiene una defensa de :"+ defensaTanque);
+
+       System.out.println("El tanque ha recibido un daño  de "+ataqueTanque+", su cantidad de vida es :"+ (defensaTanque-ataqueTanque));
+
+      System.out.println(tanque1.getPuntosVida());tanque1.getPuntosVida();
+    
     }
 
+    
     private void llenarNaves(){
        
     }
--- src/juegosdeguerra/Tanque.java
+++ src/juegosdeguerra/Tanque.java
@@ -6,16 +6,23 @@ public class Tanque extends VehiculoGuerra {
         super(nombre, ataque, defensa);
     }
 
+
     @Override
     public int atacar() {
-        // TODO Auto-generated method stub
-        return super.atacar();
+        int sumGuerreros = 10;
+        int danioTotal = (int) ((getAtaque()* Math.random()*5)+sumGuerreros*Math.random()*5);
+        
+        return danioTotal;
     }
 
     @Override
     public int defender(int ataqueRecibido) {
-        // TODO Auto-generated method stub
-        return super.defender(ataqueRecibido);
+        int vidaTotal = getPuntosVida();
+
+        int resistenciaGuerreros = 10;
+
+        int danioRecibido = (int) (vidaTotal -((getDefensa()*Math.random()*5)+resistenciaGuerreros * Math.random()*5));
+        return danioRecibido;
     }
 
 }
