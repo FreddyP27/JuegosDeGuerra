a46b2746d065c8d1ada871eb0026135f0491071b
marcealita25 <conialto@gmail.com>
2025-02-21 18:30:27 +0100

    cambio MARCELA

---
+++ src/juegosdeguerra/Soldado.java
@@ -0,0 +1,11 @@
+package juegosdeguerra;
+
+public class Soldado extends Guerrero{
+
+    public Soldado(String nombre, String tipo, int fuerza, int resistencia) {
+        super(nombre, tipo, fuerza, resistencia);
+    }
+    
+
+
+}
